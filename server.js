bb4599db75360776f04b28ec2ec82eaba799e7db
William Yang <william.yang@oocl.com>
2016-04-28 10:52:32 +0800

    add fs

--- server.js
+++ server.js
@@ -2,39 +2,26 @@
 
 const http = require('http')
 const qs = require('querystring')
+const fs = require('fs')
+
 
 http.createServer((req, res) => {
   var url = require('url').parse(req.url),
       path = url.pathname
-      console.log(path);
   if (req.method === 'GET') {
     if (path === '/home') {
       res.setHeader('Content-Type', 'text/html')
-      res.end(`
-        <a href="/login">Login</a>
-        `)
+      fs.createReadStream('./index.html').pipe(res)
     } else if (path === '/login') {
       res.setHeader('Content-Type', 'text/html')
-      res.end(`
-        <html>
-          <body>
-            <form action="/doLogin" method="get">
-              User : <input name="user">
-              <input type="submit">
-            </form>
-          </body>
-        </html>
-        `)
+      fs.createReadStream('./login.html').pipe(res)
     } else if (path === '/doLogin'){
       console.log();
       res.setHeader('Content-Type', 'text/html')
-      res.end(`
-        <html>
-          <body>
-            Hi, ${qs.parse(url.query).user}! Login Success
-          </body>
-        </html>
-        `)
+      const user = qs.parse(url.query).user
+      fs.readFile('./success.html', (err, content) => {
+        res.end(content.toString().replace(/\${(.*)}/, user))
+      })
     } else {
       res.statusCode = '404';
       res.end();
