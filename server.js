b8b03ab15117ab7b9c3908cb98f5eef1fddeba6e
William Yang <william.yang@oocl.com>
2016-04-28 11:44:08 +0800

    work with cookies

--- server.js
+++ server.js
@@ -3,17 +3,29 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+const cookieParser = require('cookie-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 
 app.set('view engine', 'ejs')
 app.set('views', './')
 
-
+app.use((req, res, next) => {
+  if (req.cookies.itauser) {
+    req.body= {user : req.cookies.itauser};
+    next()
+  } else {
+    if (req.path !== '/login' && req.path !== '/doLogin') {
+      res.redirect('/login');
+    } else {
+      next()
+    }
+  }
+})
 app.get('/home', (req, res) => {
-  res.sendFile(__dirname +'/index.html')
+  res.render('success.ejs', {user : req.body.user})
 })
 
 app.get('/login', (req, res) => {
@@ -22,6 +34,7 @@ app.get('/login', (req, res) => {
 
 app.route('/doLogin')
   .post((req, res) => {
+  res.cookie('itauser', req.body.user)
   res.render('success.ejs', {user : req.body.user})
 })
 
